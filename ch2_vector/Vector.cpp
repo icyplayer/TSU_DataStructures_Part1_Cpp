dd855bef1d20cf38228417bf01726510de1e1f2b
icyplayer <icyplayer@163.com>
2017-04-20 09:08:48 +0800

    add Vector<T> ADT for ch2

---
+++ ch2_vector/Vector.cpp
@@ -0,0 +1,23 @@
+/*
+ * Vector.cpp
+ *
+ *  Created on: 2017年4月20日
+ *      Author: icyplayer
+ */
+
+#include "Vector.h"
+
+namespace myimpl {
+
+template<typename T>
+T Vector<T>::Vector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+//template<typename T>
+//T Vector<T>::~Vector() {
+//	// TODO Auto-generated destructor stub
+//}
+
+} /* namespace myimpl */
---
+++ ch2_vector/Vector.h
@@ -0,0 +1,26 @@
+/*
+ * Vector.h
+ *
+ *  Created on: 2017年4月20日
+ *      Author: icyplayer
+ */
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+#include <iostream>
+
+using namespace std;
+namespace myimpl {
+
+template<typename T> class Vector {
+public:
+	Vector();
+//	virtual ~Vector();
+
+	size_t size();
+
+};
+
+} /* namespace myimpl */
+
+#endif /* VECTOR_H_ */
