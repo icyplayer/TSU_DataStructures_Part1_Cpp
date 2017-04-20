a86a453a404c5ebc4f2bcc70ea0ec07a4992b180
icyplayer <icyplayer@163.com>
2017-04-20 17:18:30 +0800

    add Vector

--- ch2_vector/Vector.cpp
+++ ch2_vector/Vector.cpp
@@ -10,14 +10,154 @@
 namespace myimpl {
 
 template<typename T>
-T Vector<T>::Vector() {
-	// TODO Auto-generated constructor stub
-
+T Vector<T>::Vector(int c = DEFAULT_CAPACITY) {
+	_elem = new T[_capacity = c]; // memory allocated to this vector
+	_size = 0; // elements in vector
 }
 
-//template<typename T>
-//T Vector<T>::~Vector() {
-//	// TODO Auto-generated destructor stub
-//}
+template<typename T>
+T Vector<T>::Vector(T const * A, Rank lo, Rank hi){
+	copyFrom(A, lo, hi);
+}
+
+template<typename T>
+T Vector<T>::Vector(Vector<T> const& V, Rank lo, Rank hi){
+	copyFrom(V._elem, lo, hi); // FIXME: protected param _elem cannot be visited from outside
+}
+
+template<typename T>
+T Vector<T>::Vector(Vector<T> const& V){
+	copyFrom(V._elem, 0, V._size); // FIXME: protected param _elem cannot be visited from outside
+}
+
+template<typename T>
+void Vector<T>::copyFrom(T const * A, Rank lo, Rank hi){
+	delete [] _elem; // Is this necessary to avoid memory leak?
+	_elem = new T[_capacity = 2*(hi-lo)]; // if do more comparison on old size and new size, will it be faster?
+	_size = 0;
+	while (lo < hi){
+		_elem[_size++] = A[lo++];
+	}
+}
+
+template<typename T>
+void Vector<T>::expand(){
+	if (_size < _capacity){
+		return;
+	}
+	_capacity = max(_capacity, DEFAULT_CAPACITY);
+	T* oldElem = _elem;
+	_elem = new T[_capacity <<= 1];
+	for(int i = 0; i < _size; ++i){
+		_elem[i] = oldElem[i];
+	}
+	delete [] oldElem;
+}
+
+
+template<typename T>
+void Vector<T>::shrink(){
+	// TODO
+	return;
+}
+
+template<typename T>
+T& Vector<T>::operator[] (Rank r) const { // TODO what does "const" mean?
+	return _elem[r];
+}
+
+template<typename T>
+Rank Vector<T>::insert(Rank r, T e){
+	expand(); // No overflow risk
+	int i = _size-1, j = _size;
+	while (i >= r){
+		_elem[j--] = _elem[i--]; // elements from rank=r shift right
+	}
+	_elem[r] = e;
+	_size++;
+//	expand(); // Ref: video 02C-3
+	return r;
+}
+// O(n)
+template<typename T>
+int Vector<T>::remove(Rank lo, Rank hi) {
+	if (lo == hi){
+		return;
+	}
+	while (hi < _size){
+		_elem[lo++] = _elem[hi++];
+	}
+	_size = lo;
+//	shrink();//update _capacity, if necessary.
+	return hi-lo; // Note: hi, lo are updated
+}
+
+template<typename T>
+T Vector<T>::remove(Rank r) {
+	T e = _elem[r]; // Save a copy of removed element
+	remove(r, r+1);
+	return e;
+}
+
+// O(n)
+// Input-sensitive
+template<typename T>
+Rank Vector<T>::find(T const& e ,Rank lo, Rank hi) {
+	while ((lo < hi--) && (_elem[hi] != e)); // reverse lookup
+	return hi; // hi<lo, find failed; o.w, hi is e's rank
+}
+
+template<typename T>
+int Vector<T>::deduplicate(){
+	Rank oldSize = _size;
+	Rank i = 1; // Start from _elem[1]
+	while (i < _size){
+		(find(_elem[i], 0, i) < 0) ?
+			i++ // No duplicate, check _elem[i]'s success
+			: remove(i); // o.w, remove the duplicated element
+	}
+	return oldSize - _size;
+}
+
+// Function pointer
+template<typename T>
+void Vector<T>::traverse(void (*visit)(T&)){
+	for (Rank i = 0; i < _size; ++i){
+		visit(_elem[i]);
+	}
+}
+
+// Function object
+template<typename T> template<typename VST>
+void Vector<T>::traverse(VST& visit){
+	for (Rank i = 0; i < _size; ++i){
+		visit(_elem[i]);
+	}
+}
+
+
+
+
+/* Iterating example */
+template<typename T>
+void Vector<T>::increase(Vector<T> &V){
+	V.traverse(Increase<T>());
+}
+
+/* Iterating practices */
+template<typename T>
+void Vector<T>::decrease(Vector<T> &V){
+	V.traverse(Decrease<T>());
+}
+
+template<typename T>
+void Vector<T>::doubleOpt(Vector<T> &V){
+	V.traverse(DoubleOpt<T>());
+}
+
+template<typename T>
+void Vector<T>::sum(Vector<T> &V){
+	V.traverse(Sum<T>());
+}
 
 } /* namespace myimpl */
--- ch2_vector/Vector.h
+++ ch2_vector/Vector.h
@@ -12,14 +12,93 @@
 using namespace std;
 namespace myimpl {
 
-template<typename T> class Vector {
-public:
-	Vector();
-//	virtual ~Vector();
+typedef int Rank;
 
-	size_t size();
+#define DEFAULT_CAPACITY 3
+
+
+template<typename T> class Vector {
+protected:
+	Rank _size;
+	int _capacity;
+	T* _elem;
+protected:
+	void copyFrom(T const * A, Rank lo, Rank hi);
+	void expand();
+	void shrink();
+public:
+	Vector(int c);
+	Vector(T const * A, Rank lo, Rank hi); // Copy from array
+	Vector(Vector<T> const& V, Rank lo, Rank hi); // Copy from Vector<T> V[lo, hi)
+	Vector(Vector<T> const& V); // Copy from whole Vector<T> V
+
+	~Vector(){
+		delete [] _elem;
+	}
+
+	Rank insert(Rank r, T e);
+	int remove(Rank lo, Rank hi); // remove _elem[lo, hi)
+	T remove(Rank r); // remove _elem[r] and return its value;
+	Rank find(T const& e ,Rank lo, Rank hi);
+	int deduplicate();
+	void traverse(void (*visit)(T&)); // Function pointer
+
+	template<typename VST>
+	void traverse(VST& visit);
+
+	T& operator[] (Rank r) const;// {return _elem[r];
+
+	size_t size() const {return _size;}
+
+/* Iterating examples*/
+public:
+	void increase(Vector<T> &V);
+	void decrease(Vector<T> &V);
+	void doubleOpt(Vector<T> &V);
+//	void sum(Vector<T> &V); // FIXME
+protected:
+	/* Lecture demo of ++(increase)
+	 * Modification: define the struct inside class,
+	 * where in original demo the struct is defined outside Vector<T>
+	 */
+	struct Increase{
+		virtual void operator()(T & e){ e++; } // TODO: why virtual?
+	};
+
+	/* Iterating practices */
+	struct Decrease{ // TODO: test(correctness)
+		virtual void operator()(T & e){ e--; } // TODO: why virtual?
+	};
+
+	// Double operation
+	struct DoubleOpt{ // TODO: test(correctness)
+		 // Note: operator *= may not valid
+		virtual void operator()(T & e){ e = e*2; } // TODO: why virtual?
+	};
+
+/*
+	struct Sum{ // TODO
+		virtual T operator()(Vector<T> const& V, Rank lo, Rank hi){ return sumOf(V, lo, hi); }
+		// FIXME: use traverse-like function call
+		T sumOf(Vector<T> const& V, Rank lo, Rank hi) const {
+		    if (lo == hi){
+		        return V[lo];
+		    }
+		    else{
+		        Rank mi = (lo+hi) >> 1;
+		        return sumOf(V, lo, mi) + sumOf(V, mi+1, hi);
+		    }
+		}
+	};
+*/
 
 };
+
+
+
+
+
+
 
 } /* namespace myimpl */
 
