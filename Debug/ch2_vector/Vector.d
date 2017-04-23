e93d7397a068ed904bf0afcb41d94b948af2b651
icyplayer <icyplayer@163.com>
2017-04-23 11:40:54 +0800

    delete debug folder

--- Debug/ch2_vector/Vector.d
+++
@@ -1,6 +0,0 @@
-ch2_vector/Vector.o: ../ch2_vector/Vector.cpp ../ch2_vector/Vector.h \
-  ../ch2_vector/../DataStructures/Fib.h
-
-../ch2_vector/Vector.h:
-
-../ch2_vector/../DataStructures/Fib.h:
--- Debug/ch2_vector/vectorTest.d
+++
@@ -1,6 +0,0 @@
-ch2_vector/vectorTest.o: ../ch2_vector/vectorTest.cpp \
-  ../ch2_vector/Vector.h ../ch2_vector/../DataStructures/Fib.h
-
-../ch2_vector/Vector.h:
-
-../ch2_vector/../DataStructures/Fib.h:
